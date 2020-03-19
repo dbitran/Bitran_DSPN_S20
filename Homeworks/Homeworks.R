74a3b373982d59539c2a9c0aaee1686192973e9c
dbitran <debbie.bitran@pitt.edu>
2020-03-19 11:58:59 -0400

    HW 5 in progress

--- Homeworks/Homeworks.R
+++ Homeworks/Homeworks.R
@@ -31,3 +31,61 @@ summary(model2_update)
 
 hcp.d$Gender <- as.factor(hcp.d$Gender)
 contrasts(hcp.d$Gender)
+
+##HW 5
+
+install.packages("lme4")
+library(lme4)
+library(tidyverse)
+library(boot)
+
+
+
+cbpp %>%
+  ggplot( aes(x = size, y = incidence)) +  geom_point()
+  
+
+model1 <- lm(incidence ~ size, data=cbpp)
+summary(model1)
+
+lmer1 <- lmer(incidence ~ size +  (size | herd), data=cbpp) 
+
+setwd("/Users/Debbie/Documents/DOCUMENTS/CLASSES/DSPAN/GitHub/Bitran_DSPN_S20/Homeworks")
+hcp.d <- read_csv("unrestricted_trimmed_1_7_2020_10_50_44.csv")
+
+hcp.d <- hcp.d %>%
+  select(Subject, Gender, Age, FS_IntraCranial_Vol, FS_Total_GM_Vol) %>%
+  na.omit()%>%
+  mutate(ZFS_IntraCranial_Vol = (FS_IntraCranial_Vol - mean(FS_IntraCranial_Vol))/sd(FS_IntraCranial_Vol),
+         ZFS_Total_GM_Vol = (FS_Total_GM_Vol - mean(FS_Total_GM_Vol))/sd(FS_Total_GM_Vol),
+         Gender = as.factor(Gender))%>%
+  print()
+
+log.m <- glm(Gender ~ FS_IntraCranial_Vol, data=hcp.d, family=binomial)
+summary(log.m)
+
+cv.log  <- cv.glm(hcp.d, log.m)
+summary(cv.log)
+cv.log  <- cv.glm(hcp.d, log.m, K=nrow(hcp.d))
+summary(cv.log)
+
+contrasts(hcp.d$Gender)
+log.prob.d = data.frame(predict(log.m, type="response"))
+colnames(log.prob.d) = c("predicted_prob")
+log.prob.d<- log.prob.d%>%
+  mutate(pred_gender = if_else(predicted_prob>.5, "M", "F"))
+confusion.d <- data.frame(log.prob.d$pred_gender, hcp.d$Gender)
+colnames(confusion.d) <- c("predicted", "actual")
+table(confusion.d)
+                         
+
+library(MASS)
+lda.m <- lda(Gender ~ FS_IntraCranial_Vol, data=hcp.d)
+lda.m
+plot(lda.m)
+
+lda.pred = predict(lda.m)
+lda.gender = lda.pred$class
+table(lda.gender, hcp.d$Gender)
+mean(lda.gender==hcp.d$Gender)
+
